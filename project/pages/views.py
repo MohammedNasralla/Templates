985ba6c20dace2696d32ddd35348cf8c3673a9e7
Mohammed Nasralla <m.nasralla2023@gmail.com>
2025-12-14 16:36:54 +0200

    Add Django template filters - requirement 7

--- project/pages/views.py
+++ project/pages/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def index(request):
     print("View index called")
     context = {
-        'full_name': 'Mohammed Talal Nasralla',
-        'student_id': '120212446',
-        'address': 'Gaza',
-        'email': 'mohammednasralla28@gmail.com',
-        'mobile_number': '+972598698040'
+        'first_name': 'mohammed',
+        'last_name': 'nasralla',
+        'student_id': 120212446,
+        'address': 'gaza',
+        'email': '',
+        'mobile_number': '+972598698040',
+        'date_of_birth': datetime(2003, 11, 28),
     }
     return render(request, 'pages/index.html', context)
 
